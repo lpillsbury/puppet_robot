1e46b022c30583dcbfc04ed731ecb7693457eaec
Leah Pillsbury <leahp@bu.edu>
2019-11-08 20:00:40 -0500

    created Blubbo class, modified main program

--- Blubbo/Blubbo_main.py
+++ Blubbo/Blubbo_main.py
@@ -11,6 +11,7 @@ import logging # info about how to use logging at https://realpython.com/python-
 import sys
 import random # for random number generation at a few places
 import pygame
+from Blubbo_classdef import Blubbo
 
 logging.basicConfig(level=logging.DEBUG)
 logging.debug('Starting the program')
@@ -21,15 +22,12 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 # red, green, blue eye pins, servo pin, right/left capacitive sensors
-
 r_eye = 17
 g_eye = 27
 b_eye = 22
 servoPIN = 18
 capR = 24
 capL = 23
-# define the capacitive sensor pins here or in function?
-# is function in this code or separate?
 
 GPIO.setup(g_eye,GPIO.OUT)
 GPIO.setup(b_eye,GPIO.OUT)
@@ -38,102 +36,43 @@ GPIO.setup(capR, GPIO.OUT)
 GPIO.setup(capL, GPIO.OUT)
 GPIO.setup(servoPIN, GPIO.OUT)
 
+# make blub object from Blubbo class
+blub = Blubbo()
+
 def say_greeting():
     logging.debug('starting saying greeting')
     text = "My name is Blubbo. I am a fish. I like to sing. If you touch my fin, I will sing for you"
     subprocess.call('espeak -s 100 -v en "%s" --stdout | aplay'%text, shell=True)
     logging.debug('finished saying greeting')
 
-def fart():
-    # https://raspberrypi.stackexchange.com/questions/7088/playing-audio-files-with-python
-    logging.debug('farting again')
-    which_fart = random.randint(1,8)
-    fname = 'fart' + which_fart + '.mp3'
-    # subprocess.call(aplay fname)
-    logging.debug('done farting')
-   
-def sing():
-    logging.debug('singing')
-
-def blink_eyes():
-    blink_type=random.randint(1,5)
-    keep_blinking = fart.isAlive()
-    if blink_type==1:
-        while(keep_blinking == True):
-            GPIO.output(g_eye,True)
-            GPIO.output(b_eye,True)
-            logging.debug('both eyes on and bright')
-            time.sleep(2)
-            GPIO.output(g_eye,False)
-            GPIO.output(b_eye,False)
-            logging.debug('both eyes off')
-            time.sleep(2)
-            keep_blinking = greet.isAlive()
-    elif blink_type ==2:
-        for i in range(4):
-            GPIO.output(g_eye,True)
-            GPIO.output(b_eye,False)
-            time.sleep(1)
-            GPIO.output(b_eye,True)
-            GPIO.output(g_eye,False)
-            time.sleep(0.7)
-        GPIO.output(b_eye,False)
-    elif blink_type ==3 # etc etc how many choices do I want
-
-def move_mouth(func_name):
-    # Blubbo will move his mouth when singing or introducing himself
-    p = GPIO.PWM(servoPIN, 50)  # sets PWM to frequency of 50 Hz
-    logging.debug('moving mouth in accordance with speech in:')
-    logging.debug(talk_func_name)
-    p.start(2.5) # initialization to 2.5% of duty cycle
-    # why did I choose 2.5? 
+def make_fart():
+    whichfart = random.randint(1,7)
+    logging.debug('which fart')
+    logging.debug(whichfart)
+    pygame.mixer.init()
+    pygame.mixer.music.load(blub.fart[whichfart])
+    pygame.mixer.music.play()
     
-    # puppet will only move its mouth when it says words
-    keep_talking = talk_func_name.isAlive()
-    logging.debug('keep talking=')
-    logging.debug(keep_talking)
-    while (keep_talking==True):
-        p.ChangeDutyCycle(3)
-        time.sleep(0.2)
-        p.ChangeDutyCycle(7.5)
-        p.ChangeDutyCycle(6)
-        p.ChangeDutyCycle(7.5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(2.5)
-        logging.debug('Moved mouth a cycle')
-        keep_talking = talk_func_name.isAlive()
-        logging.debug('keep talking=')
-        logging.debug(keep_talking)
-    talk_func_name.join()
-    p.stop()
-    logging.debug('Done moving mouth')
-
-greet = threading.Thread(target=say_greetings)
-say_more = threading.Thread(target=say_stuff)
-yap1 = threading.Thread(target=move_mouth, args=(greet,))
-yap2 = threading.Thread(target=move_mouth, args=(say_more,))
-greet_event = threading.Event()
-blink = threading.Thread(target=blink_eyes)
-
+def blink_eyes():
+    blink_type=random.randint(1,4)
+    keep_blinking = fart.isAlive()
+    while(keep_blinking == True):
+        logging.debug('blinking')
+        blub.blink(blink_type, r_eye, g_eye, b_eye)
+        keep_blinking = fart.isAlive()
+    fart.join()
+    logging.debug('done blinking')
+    
+# define threads
+greet = threading.Thread(target = say_greeting)
+blink = threading.Thread(target = blink_eyes)
+fart = threading.Thread(target = make_fart)
 
 def main():
-    # overlaid in all of this, we have the cap sensor saying when to do it and time elapsed ff
     greet.start()
-    yap1.start()
+    fart.start()
     blink.start()
-    yap1.join()
-    blink.join()
-    uname = get_uname()
-    # will now say hello
-    time.sleep(2)
-    logging.debug('will now respond')
-    yap2.start()
-    
-    say_more.start()
-    yap2.join()
- 
+
+
 if __name__=="__main__":
     main()
-
